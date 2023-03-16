d836ac4c7ec363bec6baba808336400d1e74bfe3
agarfar <64799305+agarfar@users.noreply.github.com>
2023-03-15 21:50:45 -0700

    fixed logic for adding posts, query posts, added PostList to channel component (#13)

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 module.exports = {
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
-
+    
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -21,7 +21,7 @@ module.exports = {
     } catch {
       console.log('Invalid token');
     }
-
+    
     return req;
   },
   signToken: function ({ email, username, _id }) {
