4b79f2e1915e272afe9754ce504856d0eb36447a
Cgotts <cobi.gottschalk@gmail.com>
2023-03-17 20:32:19 -0700

    Added FriendList file

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -2,36 +2,25 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 
-import { ADD_POST } from '../../utils/mutations';
-import { QUERY_POSTS, QUERY_ME } from '../../utils/queries';
+import { ADD_FRIEND } from '../../utils/mutations';
+import { QUERY_FRIENDS, QUERY_ME } from '../../utils/queries';
 
 import Auth from '../../utils/auth';
 
-const PostForm = () => {
-  const [postText, setPostText] = useState('');
-
-  const [characterCount, setCharacterCount] = useState(0);
-
-  const [addPost, { error }] = useMutation(ADD_POST, {
-    update(cache, { data: { addPost } }) {
+const FriendList = () => {
+  const [friend, setFriend] = useState('');
+  const [addFriend, { error }] = useMutation(ADD_FRIEND, {
+    update(cache, { data: { addFriend } }) {
       try {
-        const { posts } = cache.readQuery({ query: QUERY_POSTS });
-        console.log("Post cache", cache.readQuery({ query: QUERY_POSTS }))
+        const { friends } = cache.readQuery({ query: QUERY_FRIENDS });
+        console.log("Post cache", cache.readQuery({ query: QUERY_FRIENDS }))
         cache.writeQuery({
-          query: QUERY_POSTS,
-          data: { posts: [addPost, ...posts] },
+          query: QUERY_FRIENDS,
+          data: { friends: [addFriend, ...friends] },
         });
       } catch (e) {
         console.error(e);
       }
-
-      // update me object's cache
-      console.log("Cache error" , cache.readQuery({ query: QUERY_ME }))
-      const { me } = cache.readQuery({ query: QUERY_ME });
-      cache.writeQuery({
-        query: QUERY_ME,
-        data: { me: { ...me, posts: [...me.posts, addPost] } },
-      });
     },
   });
 
@@ -39,76 +28,27 @@ const PostForm = () => {
     event.preventDefault();
 
     try {
-      const { data } = await addPost({
+      const { data } = await addFriend({
         variables: {
-          postText,
-          postAuthor: Auth.getProfile().data.username,
+          
         },
       });
 
-      setPostText('');
+      
     } catch (err) {
       console.error(err);
     }
   };
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    if (name === 'postText' && value.length <= 280) {
-      setPostText(value);
-      setCharacterCount(value.length);
-    }
+    // const { name, value } = event.target;
   };
 
   return (
     <div>
-      <h3>What's on your techy mind?</h3>
-
-      {Auth.loggedIn() ? (
-        <>
-          <p
-            className={`m-0 ${
-              characterCount === 280 || error ? 'text-danger' : ''
-            }`}
-          >
-            Character Count: {characterCount}/280
-          </p>
-          <form
-            className="flex-row justify-center justify-space-between-md align-center"
-            onSubmit={handleFormSubmit}
-          >
-            <div className="col-12 col-lg-9">
-              <textarea
-                name="postText"
-                placeholder="Here's a new post..."
-                value={postText}
-                className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
-                onChange={handleChange}
-              ></textarea>
-            </div>
-
-            <div className="col-12 col-lg-3">
-              <button className="btn btn-primary btn-block py-3" type="submit">
-                Add Post
-              </button>
-            </div>
-            {error && (
-              <div className="col-12 my-3 bg-danger text-white p-3">
-                {error.message}
-              </div>
-            )}
-          </form>
-        </>
-      ) : (
-        <p>
-          You need to be logged in to share your posts. Please{' '}
-          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-        </p>
-      )}
+      Friends list will go here
     </div>
   );
 };
 
-export default PostForm;
+export default FriendList;
