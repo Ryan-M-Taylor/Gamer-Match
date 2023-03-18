2b4d46f2bfd629a8d30c016c91e06e229d526221
Cgotts <109563468+Cgotts1@users.noreply.github.com>
2023-03-18 14:40:31 -0700

    Code for friend button (#28)

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -42,29 +42,19 @@
 // };
 // export default FriendList;
 
-
-
-
-
-
-
-
-
-
-
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { ADD_FRIEND } from '../../utils/mutations';
-import { QUERY_FRIENDS, QUERY_ME } from '../../utils/queries';
-import Auth from '../../utils/auth';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { ADD_FRIEND } from "../../utils/mutations";
+import { QUERY_FRIENDS, QUERY_ME } from "../../utils/queries";
+import Auth from "../../utils/auth";
 const FriendList = () => {
-  const [friend, setFriend] = useState('');
+  const [friend, setFriend] = useState("");
   const [addFriend, { error }] = useMutation(ADD_FRIEND, {
     update(cache, { data: { addFriend } }) {
       try {
         const { friends } = cache.readQuery({ query: QUERY_FRIENDS });
-        console.log("Post cache", cache.readQuery({ query: QUERY_FRIENDS }))
+        console.log("Post cache", cache.readQuery({ query: QUERY_FRIENDS }));
         cache.writeQuery({
           query: QUERY_FRIENDS,
           data: { friends: [addFriend, ...friends] },
@@ -78,8 +68,7 @@ const FriendList = () => {
     event.preventDefault();
     try {
       const { data } = await addFriend({
-        variables: {
-        },
+        variables: {},
       });
     } catch (err) {
       console.error(err);
@@ -89,31 +78,24 @@ const FriendList = () => {
     // const { name, value } = event.target;
   };
 
-
   const [clickCount, setClickCount] = useState(0);
 
   // your code here
 
   const increment = () => setClickCount((prev) => prev + 1);
 
-  useEffect(()=>{
-    document.addEventListener('mousedown', increment);
+  useEffect(() => {
+    document.addEventListener("mousedown", increment);
     // Specify how to clean up after the effect:
     return () => {
-      document.removeEventListener('mousedown', increment);
+      document.removeEventListener("mousedown", increment);
     };
-  })
-
-
-
-
-
+  });
 
   return (
     <div>
       Friends list will go here
       <h1>Document Clicks: {clickCount}</h1>
-
     </div>
   );
 };
