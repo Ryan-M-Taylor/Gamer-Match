53a51a23ac666168fece73c13256d74e9e3a9987
Soprano <antfar67@gmail.com>
2023-03-18 15:34:23 -0700

    Added filter for different channels

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,8 +25,8 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_POST = gql`
-  mutation addPost($postText: String!) {
-    addPost(postText: $postText) {
+  mutation addPost($postText: String!, $postChannel: String!) {
+    addPost(postText: $postText, postChannel: $postChannel) {
       _id
       postText
       postAuthor
