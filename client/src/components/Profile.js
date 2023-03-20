4b89dba90b3abd6c08948555ab4543aef3ff1c71
Ryan Taylor <ryan.taylor1@yahoo.com>
2023-03-20 16:33:38 -0700

    pulling

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -59,7 +59,7 @@ const Profile = () => {
   return (
     <div>
       <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+        <h2 className="col-12 col-md-10 signup-form p-3 mb-5">
           {userParam
             ? `You are currently viewing ${user.username}'s Profile ${user._id}`
             : `Hi ${user.username}!`}
