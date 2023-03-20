d21e043f55c8d2914fc4898c5f3b5dfea2ba3c76
Ryan Taylor <ryan.taylor1@yahoo.com>
2023-03-19 17:14:51 -0700

    added code for server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gamer-match', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
