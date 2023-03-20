e706b22afa3bde28691a6cae0ae6bfbc61979923
Cgotts <109563468+Cgotts1@users.noreply.github.com>
2023-03-20 13:33:46 -0700

    Added some logic for delete button in friendslist, added logic for mutations, and in resolvers (#56)

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -7,28 +7,42 @@ import { useMutation } from '@apollo/client';
 import { QUERY_FRIENDS, QUERY_ME } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
 import { Container, ListGroup } from 'react-bootstrap';
+import { DELETE_FRIEND } from "../../utils/mutations";
+// import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { Navigate, useParams } from "react-router-dom";
+// import Auth from "../utils/auth";
+
+
+
 
 const FriendList = (props) => {
-  // const { loading, error, data } = useQuery(QUERY_FRIENDS, {
-  //   variables: { id: userId },
-  // });
+  
 
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error :(</p>;
+    
+    const [deleteFriend, { error }] = useMutation(DELETE_FRIEND);
 
-  // const { name, friends } = data.user;
-  console.log(props.friends)
+
+  const handleDeleteFriend = () => {
+    {props.friends.map((friend,i) => {
+      let userId = friend._id 
+      // let username = friend.username
+    return deleteFriend({variables: {friendId: userId }});}
+    )}
+
+    // deleteFriend({variables: {friendId: userId }})
+  };
+
 
   return (
     <Container>
       <div>
-        <h3>Friends:</h3>
+        <h3>Your Friends: {props.friends.length}</h3>
         {props.friends.length === 0 ? (
           <p>You have no friends yet.</p>
         ) : (
           <ListGroup>
             {props.friends.map((friend,i) => 
-              <ListGroup.Item key={`friend-list-${i}`}>{friend.username}</ListGroup.Item>
+              <ListGroup.Item key={`friend-list-${i}`}><a href={`/profiles/${friend.username}`}>{friend.username}</a> <button onClick={handleDeleteFriend}>{friend.username}{friend._id}Delete Friend</button></ListGroup.Item>
             )}
           </ListGroup>
         )}
